cc5b0bd64f4a0d50d914fc541c5ae60ee0d77734
Tomo-zhiye <oishiimono051128@gmail.com>
2020-06-28 21:36:02 +0800

    added bottom sheet

---
+++ lib/services/database.dart
@@ -0,0 +1,33 @@
+import 'package:brew_crew/screens/models/brew.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+  final CollectionReference brewCollection =
+      Firestore.instance.collection('brews');
+
+  Future updateUserData(String sugars, String name, int strength) async {
+    return await brewCollection.document(uid).setData({
+      'sugars': sugars,
+      'name': name,
+      'strength': strength,
+    });
+  }
+
+//  brew list form snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+        name: doc.data['name'] ?? '',
+        strength: doc.data['strength'] ?? 0,
+        sugars: doc.data['sugars'] ?? '0',
+      );
+    }).toList();
+  }
+
+//  get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+}
